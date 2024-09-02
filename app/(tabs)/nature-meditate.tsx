5fb7f1e5a0a118f7f06ed78f0704b31578942fa8
Patryk <patryk.swiderski.85@gmail.com>
2024-09-02 12:50:31 +0200

    Text update

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -13,7 +13,7 @@ const NatureMeditate = () => {
 			<AppGradient colors={["#161b2e", "#0a4d4a", "#766e67"]}>
 				<View className="mb-6 ">
 					<Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
-						Welcome Magda
+						Welcome Maggie
 					</Text>
 					<Text className="text-indigo-100 font-medium text-xl text-left">
 						Start your meditation practice today
